4d427c43c547e876b076eb584a6f03cccdc5af68
yah01 <kagaminehuan@gmail.com>
2020-08-27 12:39:38 +0800

    add storage usage limit

--- model/user.go
+++ model/user.go
@@ -5,13 +5,15 @@ import (
 )
 
 type User struct {
-	Id        int64     `json:"id"`
-	Username  string    `json:"username"`
-	Password  string    `json:"password"`
+	Id       int64  `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Usage    int64  `json:"usage"`
+	Cap      int64  `json:"cap"`
 	// absolute path
-	RootDir   string    `json:"root_dir"`
+	RootDir string `json:"root_dir"`
 	// relative path to RootDir
-	WorkDir   string    `json:"work_dir"`
+	WorkDir string `json:"work_dir"`
 
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
